78af613815239d8af4e9e2244845421479e46881
Mathesh Yogeswaran <matheshyogeswaran@gmail.com>
2026-03-22 09:57:37 +0000

    seperate data base for testing

--- config/db.js
+++ config/db.js
@@ -1,9 +1,17 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
+  const dbURI =
+    process.env.NODE_ENV === "test"
+      ? process.env.MONGO_TEST_URI
+      : process.env.MONGO_URI;
+
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
+    await mongoose.connect(dbURI);
+    console.log(`MongoDB Connected: ${dbURI}`);
   } catch (error) {
     console.error(error);
     process.exit(1);
